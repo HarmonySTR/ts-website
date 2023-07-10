ed403c3f9e2bbacc29a35236e61c5fcfeb28dba2
Michael Earvin Tia <mictt123@gmail.com>
2023-07-10 10:09:13 +0800

    tech savvy website

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+import Testimonies from '../components/Testimonies.vue';
+</script>
+<template>
+    <div class="background">
+      <Navbar />
+      <Testimonies />
+    </div>
+  </template>
+
+<style>
+.background
+{
+    background-color: #F5F0BB;
+}
+</style>
